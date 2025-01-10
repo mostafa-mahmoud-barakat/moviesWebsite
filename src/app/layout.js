207ab86774b741cdb46df10c1601ed88e27ff19f
mostafa-mahmoud-barakat <108579964+mostafa-mahmoud-barakat@users.noreply.github.com>
2025-01-10 16:07:56 +0200

    add search part

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./providers";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           <Navbar/>
+          <SearchBox/>
           <main>{children}</main>
         </Providers>
       </body>
